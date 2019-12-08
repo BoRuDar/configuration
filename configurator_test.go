0069da44754aff5e77e1e00dfa18fdcb60dfc5c7
Bogdan <19273017+BoRuDar@users.noreply.github.com>
2019-12-08 19:33:50 +0100

    Tests. Init.

--- configurator_test.go
+++ configurator_test.go
@@ -5,4 +5,31 @@ import (
 )
 
 func TestName(t *testing.T) {
+	cfg := struct {
+		Name     string `json:"name"          default:"defaultName"         flag:"name"`
+		LastName string `json:"last_name"     default:"defaultLastName"`
+		Age      byte   `json:"age"           env:"AGE_ENV"`
+		IsDebug  bool   `json:"is_debug"      default:"false"`
+		Obj      struct {
+			One string  `json:"one"            default:"defaultValForOne"`
+			Two float32 `json:"two"            default:"33"`
+		}
+		StrPtr  *string `json:"str_ptr"         default:"str_ptr_test"`
+		IntPtr  *int    `json:"int_ptr"         default:"123"`
+		BoolPtr *bool   `json:"bool_ptr"        default:"true"`
+	}{}
+
+	configurator, err := New(&cfg, []Provider{
+		NewFlagProvider(&cfg),
+		NewEnvProvider(),
+		NewDefaultProvider(),
+	}, false, false)
+	if err != nil {
+		panic(err)
+	}
+	if err = configurator.InitValue(); err != nil {
+		panic(err)
+	}
+
+	t.Logf("%+v", cfg)
 }
