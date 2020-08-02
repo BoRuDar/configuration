39357104b06abe6dcdc8d2a30272510f9d9271af
Bogdan <19273017+BoRuDar@users.noreply.github.com>
2020-08-02 20:42:48 +0200

    Fix tests

--- envProvider_test.go
+++ envProvider_test.go
@@ -1,55 +1,53 @@
 package configuration
 
-import "os"
+import (
+	"os"
+	"reflect"
+	"testing"
+)
 
-//import (
-//	"os"
-//	"reflect"
-//	"testing"
-//)
-//
-//func TestEnvProvider(t *testing.T) {
-//	type testStruct struct {
-//		Name string `env:"ENV_KEY1"`
-//	}
-//	testObj := testStruct{}
-//
-//	fieldType := reflect.TypeOf(&testObj).Elem().Field(0)
-//	fieldVal := reflect.ValueOf(&testObj).Elem().Field(0)
-//
-//	provider := NewEnvProvider()
-//	testValue := "ENV_VAL2"
-//
-//	removeEnvKey, err := setEnv("ENV_KEY1", testValue)
-//	if err != nil {
-//		t.Fatal("unexpected err: ", err)
-//	}
-//	defer removeEnvKey()
-//
-//	if !provider.Provide(fieldType, fieldVal) {
-//		t.Fatal("cannot set value")
-//	}
-//
-//	if !reflect.DeepEqual(testValue, testObj.Name) {
-//		t.Fatalf("\nexpected result: [%s] \nbut got: [%s]", testValue, testObj.Name)
-//	}
-//}
-//
-//func TestEnvProviderFailed(t *testing.T) {
-//	type testStruct struct {
-//		Name string `env:"ENV_KEY1"`
-//	}
-//	testObj := testStruct{}
-//
-//	fieldType := reflect.TypeOf(&testObj).Elem().Field(0)
-//	fieldVal := reflect.ValueOf(&testObj).Elem().Field(0)
-//
-//	provider := NewEnvProvider()
-//
-//	if provider.Provide(fieldType, fieldVal) {
-//		t.Fatal("must be false")
-//	}
-//}
+func TestEnvProvider(t *testing.T) {
+	type testStruct struct {
+		Name string `env:"ENV_KEY1"`
+	}
+	testObj := testStruct{}
+
+	fieldType := reflect.TypeOf(&testObj).Elem().Field(0)
+	fieldVal := reflect.ValueOf(&testObj).Elem().Field(0)
+
+	provider := NewEnvProvider()
+	testValue := "ENV_VAL2"
+
+	removeEnvKey, err := setEnv("ENV_KEY1", testValue)
+	if err != nil {
+		t.Fatal("unexpected err: ", err)
+	}
+	defer removeEnvKey()
+
+	if err := provider.Provide(fieldType, fieldVal); err != nil {
+		t.Fatalf("cannot set value: %v", err)
+	}
+
+	if !reflect.DeepEqual(testValue, testObj.Name) {
+		t.Fatalf("\nexpected result: [%s] \nbut got: [%s]", testValue, testObj.Name)
+	}
+}
+
+func TestEnvProviderFailed(t *testing.T) {
+	type testStruct struct {
+		Name string `env:"ENV_KEY1"`
+	}
+	testObj := testStruct{}
+
+	fieldType := reflect.TypeOf(&testObj).Elem().Field(0)
+	fieldVal := reflect.ValueOf(&testObj).Elem().Field(0)
+
+	provider := NewEnvProvider()
+
+	if err := provider.Provide(fieldType, fieldVal); err == nil {
+		t.Fatal("must NOT be nil")
+	}
+}
 
 func setEnv(key, val string) (func(), error) {
 	return func() {
